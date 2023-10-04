4937b64c5167d89f115ea3a99c0dc791b82f1031
zergar5 <artem_kozhanov@mail.ru>
2023-10-04 13:02:13 +0700

    Связал, но не робит

---
+++ CG1/ViewModels/PrimitivesGroupsContextView.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace CG1.ViewModels;
+
+public class PrimitivesGroupsContextView : INotifyPropertyChanged
+{
+    public ObservableCollection<PrimitivesGroupView> Views
+    {
+        get => _views;
+        set
+        {
+            _views = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private ObservableCollection<PrimitivesGroupView> _views;
+
+    public int SelectedGroupIndex
+    {
+        get => _selectedGroupIndex;
+        set
+        {
+            _selectedGroupIndex = value;
+            if (value < 0)
+            {
+                _groupsTable.UnselectAll();
+            }
+            else
+            {
+                SelectedInTableGroup = _views[value];
+                _groupsTable.SelectedIndex = value;
+            }
+            OnPropertyChanged();
+        }
+    }
+    private int _selectedGroupIndex = -1;
+
+    public PrimitivesGroupView SelectedInTableGroup
+    {
+        get => _selectedInTableGroup;
+        set
+        {
+            _selectedInTableGroup = value;
+            OnPropertyChanged();
+        }
+    }
+    private PrimitivesGroupView _selectedInTableGroup;
+
+    private readonly ListView _groupsTable;
+    private readonly MainWindow _window;
+
+    public PrimitivesGroupsContextView(ListView groupsTable, MainWindow window)
+    {
+        _groupsTable = groupsTable;
+        _window = window;
+
+        _views = new ObservableCollection<PrimitivesGroupView>();
+        _views.CollectionChanged += (_, _)
+            => OnPropertyChanged(nameof(Views));
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
